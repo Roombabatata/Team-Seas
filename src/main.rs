a2f070c36afae8860b8b089691b2d3c13df524aa
Roombabatata <fares.setbels@gmail.com>
2021-11-10 21:32:20 +0100

    Imporing Piston2d and building the window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate piston;
+extern crate graphics;
+extern crate glutin_window;
+extern crate opengl_graphics;
+
+use piston::window::WindowSettings;
+use piston::event_loop::*;
+use glutin_window::GlutinWindow;
+use opengl_graphics::{GlGraphics, OpenGL};
+
 fn main() {
-    println!("Hello, world!");
+    let opengl = OpenGL::V3_2;
+
+    let mut window: GlutinWindow = WindowSettings::new(
+        "Team Seas",
+        [200, 200]
+        ).opengl(opengl)
+            .exit_on_esc(true)
+            .build()
+            .unwrap();
 }
