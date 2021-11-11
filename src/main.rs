10ce3e6997b9ffe6528ccc6b337bc68443f29e4d
Roombabatata <fares.setbels@gmail.com>
2021-11-11 21:51:45 +0100

    New base

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,60 @@
-extern crate piston;
-extern crate graphics;
 extern crate glutin_window;
+extern crate graphics;
 extern crate opengl_graphics;
+extern crate piston;
 
-use piston::window::WindowSettings;
-use piston::event_loop::*;
-use glutin_window::GlutinWindow;
+use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop::{EventSettings, Events};
+use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
+use piston::window::WindowSettings;
+
+pub struct App {
+    gl: GlGraphics, // OpenGL drawing backend.
+}
+
+impl App {
+    fn render(&mut self, args: &RenderArgs) {
+        use graphics::*;
+
+        const BLUE: [f32; 4] = [0.0, 0.0, 1.0, 1.0];
+
+        self.gl.draw(args.viewport(), |c, gl| {
+            // Clear the screen.
+            clear(BLUE, gl);
+        });
+    }
+
+    fn update(&mut self, args: &UpdateArgs) {
+    }
+}
 
 fn main() {
+    // Change this to OpenGL::V2_1 if not working.
     let opengl = OpenGL::V3_2;
 
-    let mut window: GlutinWindow = WindowSettings::new("spinning-square", [200, 200])
+    // Create an Glutin window.
+    let mut window: Window = WindowSettings::new("Team seas", [200, 200])
         .graphics_api(opengl)
         .exit_on_esc(true)
         .build()
         .unwrap();
 
+    // Create a new game and run it.
+    let mut app = App {
+        gl: GlGraphics::new(opengl),
+        
+    };
+
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        if let Some(args) = e.render_args() {
+            app.render(&args);
+        }
+
+        if let Some(args) = e.update_args() {
+            app.update(&args);
+        }
+    }
 }
+
