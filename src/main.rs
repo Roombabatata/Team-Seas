ffc78330aa71fc193fb7338de30ae16319e2a3bf
Roombabatata <fares.setbels@gmail.com>
2021-11-19 21:12:14 +0100

    Adding the input system

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,16 @@ extern crate piston_window;
 
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{GlGraphics, OpenGL};
-use piston::event_loop::{EventSettings, Events};
-use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
+use piston::event_loop::{Events, EventSettings};
+use piston::input::{Button, Key, PressEvent, RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
-
+use piston::EventLoop;
 use glam;
 
+#[derive(Clone, PartialEq)]
 enum Direction {
     Right, Left, Up, Down
 }
-
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
     fish: Fish,
@@ -27,7 +27,7 @@ impl App {
 
         const BLUE: [f32; 4] = [0.0, 0.0, 1.0, 1.0];
 
-        self.gl.draw(args.viewport(), |c, gl| {
+        self.gl.draw(args.viewport(), |_c, gl| {
             // Clear the screen.
             clear(BLUE, gl);
         });
@@ -36,6 +36,20 @@ impl App {
 
     fn update(&mut self, _args: &UpdateArgs) {
         self.fish.update();
+    }
+    fn press(&mut self, _args: &Button) {
+        let last_direction = self.fish.dir.clone();
+        self.fish.dir = match _args {
+            &Button::Keyboard(Key::Up) 
+                if last_direction != Direction::Down => Direction::Up,
+            &Button::Keyboard(Key::Down) 
+                if last_direction != Direction::Up => Direction::Down,
+            &Button::Keyboard(Key::Right) 
+                if last_direction != Direction::Left => Direction::Right,
+            &Button::Keyboard(Key::Left) 
+                if last_direction != Direction::Right => Direction::Left,
+            _ => last_direction
+        };
     }
 }
 
@@ -60,7 +74,7 @@ impl Fish {
     }
     fn update(&mut self) {
         match self.dir {
-            Direction::Up => self.pos.x -= 1.0,
+            Direction::Up => self.pos.y -= 1.0,
             Direction::Down => self.pos.y += 1.0,
             Direction::Right => self.pos.x += 1.0,
             Direction::Left => self.pos.x -= 1.0,
@@ -70,12 +84,11 @@ impl Fish {
 }
 
 fn main() {
-    // Change this to OpenGL::V2_1 if not working.
     let opengl = OpenGL::V3_2;
 
     // Create an Glutin window.
-    let mut window: Window = WindowSettings::new("Snake Game", [620, 470])
-        .opengl(opengl)
+    let mut window: Window = WindowSettings::new("Snake Game", [200, 200])
+        .graphics_api(opengl)
         .exit_on_esc(true)
         .build()
         .unwrap();
@@ -83,10 +96,10 @@ fn main() {
     // Create a new game and run it.
     let mut app = App {
         gl: GlGraphics::new(opengl),    
-        fish : Fish {pos: glam::Vec2::new(9.0, 9.0), dir: Direction::Right},
+        fish : Fish {pos: glam::Vec2::new(0.0, 0.0), dir: Direction::Right},
     };
 
-    let mut events = Events::new(EventSettings::new());
+    let mut events = Events::new(EventSettings::new()).ups(8);
     while let Some(e) = events.next(&mut window) {
         if let Some(args) = e.render_args() {
             app.render(&args);
@@ -94,6 +107,10 @@ fn main() {
 
         if let Some(u) = e.update_args() {
             app.update(&u);
+        }
+
+        if let Some(k) = e.press_args() {
+            app.press(&k);
         }
     }
 }
